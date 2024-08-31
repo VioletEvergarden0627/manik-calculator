75d54cb396338a37da75c9119e3331195473f58d
Eric <EricLim0208@1utar.my>
2024-08-31 15:44:57 +0800

    modified docker and gradle file

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send a request to the calculator API and store the response in a variable
-response=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'num1=3&num2=5&operation=add' http://34.238.38.207:8090/manik-calculator/calculate)
+response=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'num1=3&num2=5&operation=add' http://127.0.0.1:8090/manik-calculator/calculate)
 
 # Check if the response contains the expected result
 if [[ "$response" == *"8"* ]]; then
